18b9d45b1a642ccee126a8bf38ac5ba361b7599c
Matt Johnson <mzjohnson@caleres.com>
2016-10-26 23:16:54 -0500

    margin on homepage image

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,14 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/naturalizer', 'naturalizer@index');
+
 // Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+// Route::get('auth/login', 'Auth\AuthController@getLogin');
+// Route::post('auth/login', 'Auth\AuthController@postLogin');
+// Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-Route::auth();
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Route::post('auth/register', 'Auth\AuthController@postRegister');
+// Route::auth();
 
-Route::get('/home', 'HomeController@index');
+// Route::get('/home', 'HomeController@index');
