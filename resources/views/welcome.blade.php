4c87b4d0e28b01cc7d103e8b113dca8443f7a662
Matt Johnson <mzjohnson@caleres.com>
2016-10-26 22:57:45 -0500

    header for homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Content Administration</div>
+                <div class="panel-heading">Welcome to Awesomeness</div>
 
                 <div class="panel-body">
                     <p>Ok, so each "site" needs a home page.  I guess this serves as the "home page".  Every home page should also tell a bit about the purpose.  Well, that's easy!  This is a tool to create content for various brands.</p>
