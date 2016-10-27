08c1bf0272bf545ef9e6a8d8d3d8d9fb7ba95e0f
Matt Johnson <mzjohnson@caleres.com>
2016-10-26 23:09:22 -0500

    margin on homepage image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome to Awesomeness</div>
 
                 <div class="panel-body welcome">
-                    <img src="https://source.unsplash.com/random/1300x600" alt="Random Unsplash Photo">
+                    <img src="https://source.unsplash.com/random/1300x600" height="600" alt="Random Unsplash Photo">
                     <p>Ok, so each "site" needs a home page.  I guess this serves as the "home page".  Every home page should also tell a bit about the purpose.  Well, that's easy!  This is a tool to create content for various brands.</p>
                     <p>Select a brand from the drop down (upper right hand corner of the screen).  Gotcha!  There's only one!  Jokes on you, homie!  Eventually there'll be more, but this is a test run.  Good luck!</p>
                 </div>
