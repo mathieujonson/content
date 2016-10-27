5da99f38a72438c1c37b4566a8b4b088b2c18093
Matt Johnson <mzjohnson@caleres.com>
2016-10-26 22:53:30 -0500

    home page content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Content Administration</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <p>Ok, so each "site" needs a home page.  I guess this serves as the "home page".  Every home page should also tell a bit about the purpose.  Well, that's easy!  This is a tool to create content for various brands.</p>
+                    <p>Select a brand from the drop down (upper right hand corner of the screen).  Gotcha!  There's only one!  Jokes on you, homie!  Eventually there'll be more, but this is a test run.  Good luck!</p>
                 </div>
             </div>
         </div>
