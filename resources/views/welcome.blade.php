8b6d3fc228490ab361e9c65507ba20d6cca99263
Matt Johnson <mzjohnson@caleres.com>
2016-10-26 23:01:24 -0500

    adding image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
                 <div class="panel-body">
                     <p>Ok, so each "site" needs a home page.  I guess this serves as the "home page".  Every home page should also tell a bit about the purpose.  Well, that's easy!  This is a tool to create content for various brands.</p>
                     <p>Select a brand from the drop down (upper right hand corner of the screen).  Gotcha!  There's only one!  Jokes on you, homie!  Eventually there'll be more, but this is a test run.  Good luck!</p>
+                    <p>I feel like more is needed, so check out this awesome random photo:</p>
+                    <img src="https://source.unsplash.com/random/1300x600" alt="Random Unsplash Photo">
                 </div>
             </div>
         </div>
